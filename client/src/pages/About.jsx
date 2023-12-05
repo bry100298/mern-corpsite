d047d7f7a55c0dd864df3cec0343f2d87aa89a5d
James Tin <jamestin100298@gmail.com>
2023-12-05 22:06:46 +0800

    Completing sign-up page UI

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="bg-gradient-to-b from-blue-500 to-blue-700 h-screen flex justify-center items-center">
+      <div className="max-w-4xl mx-auto text-white text-center">
+        <h1 className="text-5xl md:text-7xl font-bold mb-4">
+          Welcome to Our School
+        </h1>
+        <p className="text-lg md:text-xl mb-8">
+          Where Learning Knows No Bounds
+        </p>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+          <div className="p-6 rounded-lg bg-blue-800">
+            <h2 className="text-2xl font-semibold mb-4">Explore</h2>
+            <p className="text-lg">
+              Discover our curriculum, programs, and extracurricular activities.
+            </p>
+          </div>
+          <div className="p-6 rounded-lg bg-blue-800">
+            <h2 className="text-2xl font-semibold mb-4">Join Us</h2>
+            <p className="text-lg">
+              Enroll in our vibrant community dedicated to growth and success.
+            </p>
+          </div>
+          <div className="p-6 rounded-lg bg-blue-800">
+            <h2 className="text-2xl font-semibold mb-4">Connect</h2>
+            <p className="text-lg">
+              Engage with our faculty, students, and alumni network.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,143 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import plmunLogoCarousel from "../assets/plmunLogoCarousel.png";
+import citicsLogoCarousel from "../assets/citcs-logo.png";
+import cbaLogoCarousel from "../assets/CBA-logo.png";
+import ccjLogoCarousel from "../assets/CCJ-logo.png";
+import cteLogoCarousel from "../assets/CTE-logo.png";
+import casLogoCarousel from "../assets/CAS-logo.png";
+import gsLogoCarousel from "../assets/GS-logo.png";
+import ippgLogoCarousel from "../assets/ippg.png";
+import coaLogoCarousel from "../assets/coa.png";
+import comLogoCarousel from "../assets/com.png";
+
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default function SignUp() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>SignUp</div>
-  )
+    // <div className="p-3 max-w-lg mx-auto">
+    //   <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+    //   <form className="flex flex-col gap-4">
+    //     <input
+    //       type="text"
+    //       placeholder="username"
+    //       className="border p-3 rounded-lg"
+    //       id="username"
+    //     />
+
+    //     <input
+    //       type="text"
+    //       placeholder="email"
+    //       className="border p-3 rounded-lg"
+    //       id="email"
+    //     />
+
+    //     <input
+    //       type="password"
+    //       placeholder="password"
+    //       className="border p-3 rounded-lg"
+    //       id="password"
+    //     />
+    //     <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+    //       Sign Up
+    //     </button>
+    //   </form>
+    //   <div className="flex gap-2 mt-5">
+    //     <p>Have an account?</p>
+    //     <Link to={"/sign-in"}>
+    //       <span className="text-blue-700">Sign in</span>
+    //     </Link>
+    //   </div>
+    // </div>
+
+    // <div className="p-3 max-w-lg mx-auto bg-white shadow-lg rounded-lg mt-5">
+    <div className="p-3 flex max-w-4xl mx-auto bg-white shadow-lg rounded-lg mt-5">
+      <div className="w-1/2 pr-4">
+        <Carousel showThumbs={false} infiniteLoop autoPlay>
+          <div>
+            <img src={plmunLogoCarousel} alt="Image 1" />
+          </div>
+          <div>
+            <img src={citicsLogoCarousel} alt="Image 2" />
+          </div>
+          <div>
+            <img src={cbaLogoCarousel} alt="Image 2" />
+          </div>
+          <div>
+            <img src={ccjLogoCarousel} alt="Image 2" />
+          </div>
+          <div>
+            <img src={cteLogoCarousel} alt="Image 2" />
+          </div>
+          <div>
+            <img src={casLogoCarousel} alt="Image 2" />
+          </div>
+          <div>
+            <img src={gsLogoCarousel} alt="Image 2" />
+          </div>
+          <div>
+            <img src={ippgLogoCarousel} alt="Image 2" />
+          </div>
+          <div>
+            <img src={coaLogoCarousel} alt="Image 2" />
+          </div>
+          <div>
+            <img src={comLogoCarousel} alt="Image 2" />
+          </div>
+        </Carousel>
+      </div>
+      <div className="w-1/2 pl-4">
+        <div className="p-3 bg-white shadow-lg rounded-lg">
+          <h1 className="text-3xl text-center font-semibold my-7 text-gray-800">
+            Create an Account
+          </h1>
+          <form className="flex flex-col gap-4">
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Username"
+                className="border p-3 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                id="username"
+              />
+            </div>
+
+            <div className="relative">
+              <input
+                type="text"
+                placeholder="Email"
+                className="border p-3 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                id="email"
+              />
+            </div>
+
+            <div className="relative">
+              <input
+                type="password"
+                placeholder="Password"
+                className="border p-3 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+                id="password"
+              />
+            </div>
+            <button className="bg-blue-500 text-white p-3 rounded-lg uppercase hover:bg-blue-600 disabled:opacity-80">
+              Create Account
+            </button>
+          </form>
+          <div className="flex justify-center items-center mt-5 text-gray-700">
+            <p>Already have an account?</p>
+            <Link to={"/sign-in"} className="ml-2 text-blue-500">
+              Sign in
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
