b771fe1f3e066d27b42e618aa16330bfa01afbd7
James Tin <jamestin100298@gmail.com>
2023-12-08 10:08:37 +0800

    Improve a bit for welcome page font

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="home-bg">
       <div className="content">
-        <h1>Welcome to Your PLMUN Careers</h1>
+        <h1 className="text-5xl md:text-7xl font-bold mb-4">Welcome to Your PLMUN Careers</h1>
         {/* Other content */}
       </div>
     </div>
